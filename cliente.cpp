c379af91a16adc050469b2a74b798c2aae262665
Angel Obando <alobandof@javerianacali.edu.co>
2025-10-16 00:19:48 -0500

    UPDATE: Correction of methods in Client class

--- cliente.cpp
+++ cliente.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include "tienda.h"
 #include "cliente.h"
 
 using std::cout;
@@ -31,15 +33,36 @@ void Cliente::mostrar_compras( ) {
     return;
 }
 
-void Cliente::realizar_compra( Producto* producto, u_int cantidad ) {
-    if( cantidad > producto -> get_cantidad( ) ) {
+Producto* Cliente::escoger_producto( Tienda* tienda, Producto* producto, u_int cantidad ) {
+    vector<Producto*>::iterator iter = find( tienda -> mostrar_productos( ).begin( ), tienda -> mostrar_productos( ).end( ), producto );
+    if( iter != tienda -> mostrar_productos( ).end( ) ) {
+        return producto;
+    } else {
+        cout << "El producto no esta en Stock." << endl;
+    }
+
+    return NULL;
+}
+
+/*
+void Cliente::realizar_compra( Tienda* tienda ) {
+    cout << "====== Productos de la tienda ======" << endl;
+    for( int i = 0; i < tienda -> mostrar_productos( ).size( ); i++ ) {
+        Producto* actual = tienda -> mostrar_productos( )[ i ];
+        if( actual -> get_cantidad( ) > 0 ) {
+            cout << actual -> get_id( ) << ". " << actual -> get_nombre( ) << ", Cantidad: " << actual -> get_cantidad( ) << endl;  
+        }
+    }
+
+    if( ) {
         cout << "No hay suficientes cantidades del producto " << producto -> get_nombre( ) << " en Stock." << endl;
         return;
     } else {
         Venta* nueva_venta = new Venta;
-        nueva_venta -> nueva_venta( this, producto, cantidad );
+        nueva_venta -> nueva_venta( tienda, this );
         this -> compras.push_back( nueva_venta );
     }
-    
+
     return;
 }
+*/
